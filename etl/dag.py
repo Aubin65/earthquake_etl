a0d9fca49bb70bdbe53caaec97668f06840a0ada
Aubin65 <amorlas65@gmail.com>
2024-11-25 11:59:03 +0100

    Ajout de la fonction connect_mongo() et commencement de la fonction d'extraction

--- etl/dag.py
+++ etl/dag.py
@@ -17,20 +17,83 @@ def superheroes_etl():
     """DAG global d'import des données des tremblement de terre depuis le fichier csv des données brutes vers la base de données MongoDB"""
 
     @task
-    def extract(request: str) -> pd.DataFrame:
+    def connect_mongo(
+        mongo_client: str = "mongodb://localhost:27017/",
+        mongo_db: str = "earthquake_db",
+        mongo_collection: str = "earthquakes",
+    ) -> tuple:
+        """_summary_
+
+        Parameters
+        ----------
+        mongo_client : _type_, optional
+            url de connexion à la base de données MongoDB, by default "mongodb://localhost:27017/"
+        mongo_db : str, optional
+            _description_, by default "earthquake_db"
+        mongo_collection : str, optional
+            _description_, by default "earthquakes"
+
+        Returns
+        -------
+        tuple
+            Tuple contenant le client mongo, la db et la collection pour requêter la base de données
+        """
+
+        client = pymongo.MongoClient(mongo_client)
+        db = client["earthquake_db"]
+        collection = db["earthquakes"]
+
+        return client, db, collection
+
+    @task
+    def extract(
+        client: str,
+        db: str,
+        collection: str,
+        request: str = "https://earthquake.usgs.gov/fdsnws/event/1/query?",
+        format: str = "geojson",
+        starttime: str = "2024-11-01",
+    ) -> pd.DataFrame:
         """Tâche d'extraction de la donnée depuis l'API
 
         Parameters
         ----------
-        request : str
-            Corps de la requête à effectuer
+        request : _type_, optional
+            corps de la requête à l'API, by default "https://earthquake.usgs.gov/fdsnws/event/1/query?"
+        format : str, optional
+            format de la réponse attendu, by default "geojson"
+        starttime : str, optional
+            paramètre de la requête, by default "2024-11-01"
+        client : str
+            client de la base de données, issu de la fonction connect_mongo()
+        db : str
+            nom de la base de données, issu de la fonction connect_mongo()
+        collection : str
+            nom de la collection, issu de la fonction connect_mongo()
 
         Returns
         -------
         pd.DataFrame
-            DataFrame comprenant les données brutes extraites de l'API
+            DataFrame contenant les résultats de la requête API
         """
-        pass
+
+        # -------------------------------------------------------------------------------------------------------------------------------------------#
+        # Phase de récupération de la dernière date de requête
+
+        mongo_request = [
+            {"$project": {"generated": "$metadata.generated"}},
+            {"$group": {"_id": None, "maxGenerated": {"$max": "$generated"}}},
+        ]
+
+        res = list(collection.aggregate(mongo_request))
+
+        starttime = res[0]["maxGenerated"]
+
+        # -------------------------------------------------------------------------------------------------------------------------------------------#
+        # Requête API
+        final_api_request = f"{request}format={format}&starttime={starttime}"
+
+        return requests.get(final_api_request).json()
 
     @task
     def transform(raw_df: pd.DataFrame) -> pd.DataFrame:
