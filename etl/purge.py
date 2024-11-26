16c4a5045d69b52c345db88dc1b1d720d4f2422c
Aubin65 <amorlas65@gmail.com>
2024-11-26 15:02:10 +0100

    Changement de days_ago de airflow vers pendulum

--- etl/purge.py
+++ etl/purge.py
@@ -5,11 +5,11 @@ Ce fichier est utilisé pour mettre en place la purge de la base de données des
 # Import des librairies nécessaires
 from airflow.decorators import dag, task  # noqa
 from airflow.exceptions import AirflowException  # noqa
-from airflow.utils.dates import days_ago  # noqa
 import pymongo  # noqa
 import pymongo.collection  # noqa
 import requests  # noqa
 from datetime import datetime, timezone  # noqa
+import pendulum
 
 # DAG de base
 default_args = {
@@ -22,7 +22,7 @@ default_args = {
 @dag(
     schedule="*/1 * * * *",  # Exécution toutes les minutes
     default_args=default_args,
-    start_date=days_ago(1),
+    start_date=pendulum.today("UTC").add(days=-1),
     max_active_runs=1,  # Ici on définit ce paramètre à 1 pour empêcher les doublons d'exécution de ce DAG
     catchup=False,
     tags=["purge_earthquake_db"],
