7bad47be72b001202af413159c3f5d77ff11262c
Aubin65 <amorlas65@gmail.com>
2024-11-29 14:49:43 +0100

    Ajout de fonctions de tests du premier DAG -> etl.py

---
+++ tests/etl_integration_test.py
@@ -0,0 +1,70 @@
+"""
+Ce script python est utilisé pour tester les DAGs et fonctions mises en place dans ce projet
+"""
+
+# Import des librairies nécessaires
+from airflow.models import DagBag
+from airflow.utils.state import State
+
+
+def test_dag_loading():
+    """
+    Fonction de test sur le chargement du DAGs
+    Source : https://www.restack.io/docs/airflow-knowledge-apache-unit-testing
+    """
+
+    # Récupération du DAG
+    dag = DagBag()
+
+    # Vérifier que le DAG existe
+    assert dag is not None, "DAG inexistant"
+
+    # Test sur les erreurs du DAG
+    assert len(dag.import_errors) == 0, "No DAG loading errors"
+
+    # Vérifier les tâches attendues du DAG
+    assert dag.task_ids == {"extract", "load", "transform"}, "Tâches non conformes"
+
+
+# Test d'intégration (source : https://www.restack.io/docs/airflow-knowledge-apache-unit-testing)
+def test_task_dependencies():
+
+    # Récupération du DAG
+    dag = DagBag().get_dag("earthquake_etl")
+
+    # Récupérer ces tâches
+    tasks = dag.tasks
+
+    # Initialisation des dépendances attendues
+    dependencies = {
+        "extract": {"downstream": ["transform"], "upstream": []},
+        "transform": {"downstream": ["load"], "upstream": ["extract"]},
+        "load": {"downstream": [], "upstream": ["transform"]},
+    }
+
+    # Tests sur les dépendances
+    for task in tasks:
+        assert task.downstream_task_ids == set(
+            dependencies[task.task_id]["downstream"]
+        ), "Dépendances downstream non correspondantes"
+        assert task.upstream_task_ids == set(
+            dependencies[task.task_id]["upstream"]
+        ), "Dépendances upstream non correspondantes"
+
+
+# End-to-end test
+def test_full_dag_execution():
+
+    # Récupération du DAG
+    dag = DagBag().get_dag("earthquake_etl")
+
+    # Nettoyage des exécutions passées
+    dag.clear()
+
+    # Lancement du DAG
+    dag.run()
+
+    # Tests des états des différentes tâches
+    assert all(
+        task_instance.state == State.SUCCESS for task_instance in dag.get_task_instances()
+    ), "Toutes les tâches ne sont pas un succès"
--- tests/etl_unit_test.py
+++ tests/etl_unit_test.py
@@ -3,3 +3,9 @@ Ce script python est utilisé pour tester les DAGs et fonctions mises en place d
 """
 
 # Import des librairies nécessaires
+from airflow.models import DagBag  # noqa
+from airflow.utils.state import State  # noqa
+
+
+def test_extract_task():
+    pass
